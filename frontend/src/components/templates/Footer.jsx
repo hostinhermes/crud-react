3ef1b115ef70727b42940b1113a757b4de410084
Hermes Hostin <hermeshostin@gmail.com>
2020-07-25 22:22:11 -0300

    commit inicial

---
+++ frontend/src/components/templates/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Herm<span className="text-danger">e</span>s</strong>
+            <strong> Host<span className="text-danger">i</span>n</strong>
+
+        </span>
+    </footer>
